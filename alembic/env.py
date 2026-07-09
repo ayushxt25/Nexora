9c56ead1023389e869f74158483277dddecd1127
Ayush <ayushgiri2125@gmail.com>
2026-07-09 15:09:08 +0530

    Escape DATABASE_URL for Alembic config

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,7 @@ from app.database import Base
 from app import db_models  # noqa: F401
 
 config = context.config
-config.set_main_option("sqlalchemy.url", get_database_url())
+config.set_main_option("sqlalchemy.url", get_database_url().replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
